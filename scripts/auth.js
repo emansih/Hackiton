77b1491cca60251d816d5efa0475d990ece0b917
emansih <danielquah148@yahoo.com>
2021-04-03 09:30:18 +1100

    add authentication

---
+++ scripts/auth.js
@@ -0,0 +1,36 @@
+var auth = new firebaseui.auth.AuthUI(firebase.auth());
+auth.start('#auth-container', {
+    signInOptions: [
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID
+    ],
+});
+
+var uiConfig = {
+    callbacks: {
+        signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+            return true;
+        },
+        uiShown: function() {
+            document.getElementById('loading').style.display = 'none';
+        }
+    },
+    signInFlow: 'popup',
+    signInSuccessUrl: 'dashboard.html',
+    signInOptions: [
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    ]
+};
+auth.start('#auth-container', uiConfig);
+firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+        setCookie('userId', user.uid)
+    }
+});
+
+function setCookie(name,value) {
+    var expires = "";
+    var date = new Date();
+    date.setTime(date.getTime() + (24*60*60*1000));
+    expires = "; expires=" + date.toUTCString();
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
---
+++ scripts/firebaseInit.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAwxcHuZwm-XSyKtd2nkuWQPc9du7Vn0ck",
+    authDomain: "hackiethon.firebaseapp.com",
+    projectId: "hackiethon",
+    storageBucket: "hackiethon.appspot.com",
+    messagingSenderId: "396646079335",
+    appId: "1:396646079335:web:f9fb47dfc6ddbd54e4adc2"
+};
+
+firebase.initializeApp(firebaseConfig);
