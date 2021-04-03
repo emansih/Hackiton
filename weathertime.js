89bdf7f133bf5cf5b43ce3722051eb8971489a60
echan1912 <51402884+echan1912@users.noreply.github.com>
2021-04-03 16:45:44 +0800

    Add files via upload
    please check my javascript and adapt my HTML files, js and apis aren't loading, cry

---
+++ weathertime.js
@@ -0,0 +1,32 @@
+// set date and time
+const now = new Date()
+//shows user date
+now ;
+
+//have user set city
+GET api.openweathermap.org/data/2.5/weather?id=2158177&units=metric&appid={2d6333bd76970c4b1d16306b517f334c}
+
+//importing openweathermap api
+// reference https://stackoverflow.com/questions/36174177/show-local-weather-using-openweathermap-api
+
+var getWeather = function(data) {
+    $.getJSON('http://api.openweathermap.org/data/2.5/weather', {
+        lat: data.lat,
+        lon: data.lon,
+        units: "metric",
+        appid: "2d6333bd76970c4b1d16306b517f334c"
+    }, showWeather, 'jsonp');
+};
+
+var showWeather = function(data) {
+    $("#test").text("this shows local weather")
+    $("#temp").text(data.main.temp)
+    $("#description").text(data.weather[0].description)
+    $("#place").text(data.name)
+};
+
+$(document).ready(function() {
+    $("#btn").click(function() {
+        $.getJSON('http://ip-api.com/json', getWeather)
+    })
+})
