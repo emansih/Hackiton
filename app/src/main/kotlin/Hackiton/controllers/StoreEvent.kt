0c9cbd29608de7f34c3e8705681456eacc9e5dca
emansih <danielquah148@yahoo.com>
2021-04-04 18:56:37 +1000

    Remove datetime-local because some browsers don't support it

--- app/src/main/kotlin/Hackiton/controllers/StoreEvent.kt
+++ app/src/main/kotlin/Hackiton/controllers/StoreEvent.kt
@@ -10,13 +10,14 @@ import java.time.ZoneId
 class StoreEvent: Handler {
     override fun handle(ctx: Context) {
         val startTime: String = ctx.formParam("startTime") ?: ""
+        val startDate: String = ctx.formParam("startDate") ?: ""
         val description: String = ctx.formParam("description") ?: ""
         val db = FirestoreClient.getFirestore()
         val docRef = db.collection("calendar")
         val calendarRef = docRef.document()
         val zone = ZoneId.of("Australia/Melbourne")
         val zoneOffSet = zone.rules.getOffset(LocalDateTime.now())
-        val epochSecond = LocalDateTime.parse(startTime).toEpochSecond(zoneOffSet)
+        val epochSecond = LocalDateTime.parse(startDate + "T" + startTime).toEpochSecond(zoneOffSet)
         calendarRef.create(CalendarItems(ctx.cookie("userId") ?: "", calendarRef.get().get().id,
                 description, epochSecond.toString()))
         ctx.redirect("/dashboard")
