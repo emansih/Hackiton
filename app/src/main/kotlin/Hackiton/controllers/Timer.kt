5f797d1ab78c742f82eee3ff27b4d15ae5042a4c
emansih <danielquah148@yahoo.com>
2021-04-04 22:47:04 +1000

    update timer page

--- app/src/main/kotlin/Hackiton/controllers/Timer.kt
+++ app/src/main/kotlin/Hackiton/controllers/Timer.kt
@@ -1,10 +1,26 @@
 package Hackiton.controllers
 
+import com.google.firebase.cloud.FirestoreClient
 import io.javalin.http.Context
 import io.javalin.http.Handler
+import java.time.LocalDate
+import java.time.ZoneId
 
 class Timer: Handler {
+
+    private val hashMap: HashMap<String, Any> = HashMap()
+
+
     override fun handle(ctx: Context) {
-        ctx.render("/views/timer.html")
+        val db = FirestoreClient.getFirestore()
+        val rnds = (1..10).random()
+        db.collection("quotes").listDocuments().forEach {  docRef ->
+            hashMap["quotes"] = docRef.get().get().get(rnds.toString()) as String
+        }
+        val localDate = LocalDate.now(ZoneId.of("Australia/Melbourne"))
+        val parsedDate = localDate.dayOfMonth.toString() + " " + localDate.month + " " + localDate.year
+        hashMap["date"] = parsedDate
+
+        ctx.render("/views/timer.html", hashMap)
     }
 }
